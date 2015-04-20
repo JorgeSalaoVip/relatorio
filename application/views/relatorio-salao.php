31ff9283b6f84e2ea7f5c33ca85103a326effdb7
JorgeSalaoVip <jorge@salaovip.com.br>
2015-04-20 17:27:53 -0300

    validação front-end

--- application/views/relatorio-salao.php
+++ application/views/relatorio-salao.php
@@ -11,7 +11,13 @@
         <link rel="stylesheet" href="<?php echo base_url('public/css/relatorio.css')?>">
         <script type="text/javascript" src="<?php echo base_url('public/js/jquery.min.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url('public/js/bootstrap.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('public/js/plugins/maskedinput.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('public/js/plugins/jquery.numeric.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('public/js/plugins/parsley.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('public/js/plugins/pt-br.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('public/js/user_plugins/loadUtils.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url('public/js/site/relatorio.js'); ?>"></script>
+        <script> function base_url(url){ return '<?php echo base_url();?>'+url; }</script>
         
     </head>
     <body>
@@ -26,13 +32,28 @@
                 Apresentação
             </div>
             
-            <form>
+            <form id="form" data-parsley-validate>
                 
                 <div id="questionario">
                     
                     <ul id="q1" class="questao">
                         <li class="descricao">
                             você teste?
+                        </li>
+                        <li>
+                            <label class="checkbox">
+                                <input type="checkbox" name="e" data-parsley-mincheck="1" /> Check me out
+                            </label>
+                        </li>
+                        <li>
+                            <label class="checkbox">
+                                <input type="checkbox" name="e" /> Check me out
+                            </label>
+                        </li>
+                        <li>
+                            <label class="checkbox">
+                                <input type="checkbox" name="e" /> Check me out
+                            </label>
                         </li>
                         <li>
                             <label class="checkbox">
@@ -57,7 +78,7 @@
                         </li>
                         <li>
                             <label class="radio">
-                                <input type="radio" name="b" />
+                                <input type="radio" name="b" required />
                                 radio teste
                             </label>
                         </li>
@@ -80,18 +101,18 @@
                             você teste num 1?
                         </li>
                         <li>
-                            <input type="number" min="0" name="c" />
+                            <input type="number" min="0" name="c" required />
                         </li>
                     </ul>
                     
-                    <div id="q4" class="questao">
+                    <ul id="q4" class="questao">
                         <li class="descricao">
                             você teste num 2?
                         </li>
                         <li>
-                            <input type="number" min="0" name="d" />
+                            <input type="number" min="0" name="d" required />
                         </li>
-                    </div>
+                    </ul>
                     
                     <ul id="q5" class="questao">
                         <li class="descricao">
@@ -99,7 +120,7 @@
                         </li>
                         <li>
                             <label class="checkbox">
-                                <input type="checkbox" name="e" /> Check me out
+                                <input type="checkbox" name="e" data-parsley-mincheck="1" /> Check me out
                             </label>
                         </li>
                         <li>
@@ -120,7 +141,7 @@
                     <div class="control-group">
                         <label class="control-label" for="inputNome">Nome</label>
                         <div class="controls">
-                            <input type="text" id="inputNome">
+                            <input type="text" id="inputNome" required>
                         </div>
                     </div>
                     
@@ -128,7 +149,7 @@
                     <div class="control-group">
                         <label class="control-label" for="inputEmail">E-mail</label>
                         <div class="controls">
-                            <input type="text" id="inputEmail">
+                            <input type="email" id="inputEmail" data-parsley-trigger="change" required>
                         </div>
                     </div>
                     
@@ -136,7 +157,7 @@
                     <div class="control-group">
                         <label class="control-label" for="inputTelefone">Telefone</label>
                         <div class="controls">
-                            <input type="text" id="inputTelefone">
+                            <input type="text" id="inputTelefone" required>
                         </div>
                     </div>
                 </div>
--- public/js/site/relatorio.js
+++ public/js/site/relatorio.js
@@ -2,21 +2,44 @@ function Relatorio() {
     var _self = this;
     
     
+    _self.limitarCheck = function(obj, qtd) {
+        obj.find('input').each(function() {
+            $(this).bind('change', function() {
+                var checados = obj.find('input:checked').length;
+
+                if(checados > qtd) {
+                    $(this).prop('checked', false);
+                }
+            });
+        });
+    };
+    
+    _self.configurarEnvio = function(objForm, objBt) {
+        $(objForm).parsley();
+        
+        $(objBt).click(function() {
+            if($(objForm).parsley().validate()) {
+                //_self.gerarRelatorio();
+                alert('ok');
+            }
+        });
+    };
     
     
-    _self.ajaxExemplo = function(dados) {
+    _self.gerarRelatorio = function(dados) {
         loadUtils.abrir();
         
         $.ajax({
             type: "POST",
             data: 'dados=' + JSON.stringify(dados),
             dataType : "json",
-            url: base_url('balbal/gravar'),
+            url: base_url('relatorio/gerar'),
             success : function(dados){
                 loadUtils.remover();
+
                 //var obj = JSON.parse(json_str);
                 if(dados.feedBack.erro == true){
-                    apprise(dados.feedBack.erroDescricao);
+                    alert(dados.feedBack.erroDescricao);
                 } else if(!_self.isDuploClique(dados.feedBack)) {
                     
                 }
@@ -25,7 +48,12 @@ function Relatorio() {
     };
     
     _self.construtor = function() {
-        alert('foi');
+        _self.limitarCheck($('#q1'), 3);
+	$("#inputTelefone").mask("(99) 9999-9999?9");
+	$("#q3 input, #q4 input").numeric();
+        
+        _self.configurarEnvio('#form', '#submitBt');
+        
     };
     
     _self.construtor();
