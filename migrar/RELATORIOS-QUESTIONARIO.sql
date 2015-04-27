1e8c22b8edeaa3fa798f45173781da23a6e14093
JorgeSalaoVip <jorge@salaovip.com.br>
2015-04-27 17:29:49 -0300

    questões de banco
    sql exportado

--- migrar/RELATORIOS-QUESTIONARIO.sql
+++ migrar/RELATORIOS-QUESTIONARIO.sql
@@ -27,14 +27,14 @@ DROP TABLE IF EXISTS `relatorios`;
 CREATE TABLE `relatorios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `QA` varchar(10) DEFAULT NULL,
-  `QB` varchar(10) DEFAULT NULL,
-  `QC` varchar(10) DEFAULT NULL,
-  `QD` varchar(10) DEFAULT NULL,
-  `QE` varchar(10) DEFAULT NULL,
-  `outro` varchar(60) DEFAULT NULL,
+  `QA` varchar(40) DEFAULT NULL,
+  `QB` varchar(20) DEFAULT NULL,
+  `QC` varchar(20) DEFAULT NULL,
+  `QD` varchar(20) DEFAULT NULL,
+  `QE` varchar(40) DEFAULT NULL,
+  `outro` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +43,7 @@ CREATE TABLE `relatorios` (
 
 LOCK TABLES `relatorios` WRITE;
 /*!40000 ALTER TABLE `relatorios` DISABLE KEYS */;
+INSERT INTO `relatorios` VALUES (1,2,'A2A3A7','B1','7','2','E3E5','legal'),(2,3,'A2,A3,A7','B1','7','2','E3,E5','legal'),(3,4,'A3,A6,A7','B3','4','1','E2,E6','apenas um teste'),(4,5,'A2,A4','B2','7','1','E4,E5,E6,E',''),(5,6,'A2,A6','B1','8','2','E2,E3,E4',''),(6,7,'A2,A3','B1','1','2','E1,E3',''),(7,8,'A2','B2','4','2','E3,E5',''),(8,9,'A2,A3','B2','5','1','E1,E2,E3,E6,E7',''),(9,10,'A7','B3','5','1','E1,E2,E3,E6,E7',''),(10,11,'A7','B3','5','1','E9','');
 /*!40000 ALTER TABLE `relatorios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +60,7 @@ CREATE TABLE `usuarios` (
   `email` varchar(60) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +69,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Jorge','jorge@salaovip.com.br','(11) 9287-23872'),(2,'July','jorge@salaovip.com.br','(11) 9993-23222'),(3,'July','jorge@salaovip.com.br','(11) 9993-23222'),(4,'Adriano','jorge@salaovip.com.br','(11) 9383-83838'),(5,'Severino','jorge@salaovip.com.br','(11) 8348-7534'),(6,'Wesleywords','jorge@salaovip.com.br','(11) 9383-83823'),(7,'dsgffdg','jorge@salaovip.com.br','(34) 2342-34234'),(8,'Um cara do teste','jorge@salaovip.com.br','(11) 9383-83487'),(9,'Fernanda','fernanda.greggio@gmail.com','(11) 9947-27470'),(10,'Fernanda','fernanda.gregg@mail.com','(11) 9947-27470'),(11,'victor','victor@wiklive.com.br','(11) 1111-11111');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-22 12:01:31
+-- Dump completed on 2015-04-27 17:27:27
